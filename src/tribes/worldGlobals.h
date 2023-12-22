c0b203a1795df6f797ce5a2ce55cad6d12b72ff0
AltimorTASDK <altimormc@gmail.com>
2023-12-22 02:15:27 -0500

    add lastTime

--- src/tribes/worldGlobals.h
+++ src/tribes/worldGlobals.h
@@ -7,8 +7,9 @@ class SimManager;
 
 class WorldGlobals {
 public:
-	FIELD(0x04, unsigned int, currentTime);
-	FIELD(0x08, unsigned int, timeBase);
+	FIELD(0x00, uint32_t, lastTime);
+	FIELD(0x04, uint32_t, currentTime);
+	FIELD(0x08, uint32_t, timeBase);
 	FIELD(0x14, SimManager*, manager);
 	FIELD(0x1C, PlayerPSC*, psc);
 };
