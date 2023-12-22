c7b08d0e045955d8b4e5d75bea7115551f090cf0
AltimorTASDK <altimormc@gmail.com>
2023-12-21 19:11:26 -0500

    reformat, add operator[]

--- src/darkstar/Core/tVector.h
+++ src/darkstar/Core/tVector.h
@@ -14,17 +14,21 @@ class Vector {
 	T *array;
 
 public:
-	using value_type = T;
-	using reference = T&;
+	using value_type      = T;
+	using reference       = T&;
 	using const_reference = const T&;
-
-	using iterator = T*;
-	using const_iterator = const T*;
+	using iterator        = T*;
+	using const_iterator  = const T*;
 	using difference_type = int;
-	using size_type = int;
+	using size_type       = int;
 
-	iterator begin() { return &array[0]; }
-	const_iterator begin() const { return &array[0]; }
-	iterator end() { return &array[element_count]; }
-	const_iterator end() const { return &array[element_count]; }
+	iterator        begin()                     { return &array[0]; }
+	const_iterator  begin()               const { return &array[0]; }
+	iterator        end()                       { return &array[element_count]; }
+	const_iterator  end()                 const { return &array[element_count]; }
+	reference       operator[](int index)       { return array[index]; }
+	const_reference operator[](int index) const { return array[index]; }
+
+	int  size()  const { return element_count; }
+	bool empty() const { return element_count == 0; }
 };
