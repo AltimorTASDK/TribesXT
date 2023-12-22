1baf1661587630985faeb8cf40a5e0be5da3a2ad
AltimorTASDK <altimormc@gmail.com>
2023-12-21 21:37:31 -0500

    use const ref for easier passing

--- src/darkstar/Ml/processor.h
+++ src/darkstar/Ml/processor.h
@@ -28,7 +28,7 @@ private:
 	virtual void vfunc10() const;
 	virtual void vfunc11() const;
 public:
-	virtual void m_TMat3F_TMat3F_mul(const TMat3F*, const TMat3F*, TMat3F*) const;
+	virtual void m_TMat3F_TMat3F_mul(const TMat3F&, const TMat3F&, TMat3F*) const;
 private:
 	virtual void vfunc13() const;
 	virtual void vfunc14() const;
