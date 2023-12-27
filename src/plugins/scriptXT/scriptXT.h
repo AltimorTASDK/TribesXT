7f0df419ea541f93f7491dcf08b03db88b94ce98
AltimorTASDK <altimormc@gmail.com>
2023-12-27 03:39:35 -0500

    fix right justify logic in gui text

--- src/plugins/scriptXT/scriptXT.h
+++ src/plugins/scriptXT/scriptXT.h
@@ -25,8 +25,22 @@ private:
 
 	struct {
 		struct {
+			// Set new player info variables
 			StaticJmpHook<0x48BC40, hook_FearPlugin_endFrame> endFrame;
 		} FearPlugin;
+		struct {
+			// Fix right alignment with multiple formatting segments
+			StaticCodePatch<0x55367F, "\xEB"> formatControlString_fixRightAlign;
+		} TextFormat;
+		struct {
+			// Right align based on text control width rather than parent width
+			// push edi
+			// mov edi, [esi+0x1F8]
+			// jmp 0x478E0E
+			StaticCodePatch<0x478DF6, "\x57\x8B\xBE\xF8\x01\x00\x00\xEB\x0F"> setScriptValue_fixExtent;
+			// ret 8
+			StaticCodePatch<0x478E60, "\xC2\x08\x00"> parentResized_fixExtent;
+		} FearGuiFormattedText;
 	} hooks;
 
 public:
