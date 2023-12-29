880482aec6d99b6be24a70d317148a2c40c9a6d0
AltimorTASDK <altimormc@gmail.com>
2023-12-29 05:54:03 -0500

    add current time stuff from SimGame

--- src/darkstar/Sim/simGame.h
+++ src/darkstar/Sim/simGame.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "util/meta.h"
+#include "util/struct.h"
 
 class SimConsolePlugin;
 
@@ -11,9 +12,21 @@ public:
 		return *(SimGame**)0x7510D8;
 	}
 
+	FIELD(0x1F0, double, frameStart);
+
 	void registerPlugin(SimConsolePlugin *pl)
 	{
 		using func_t = to_static_function_t<decltype(&SimGame::registerPlugin)>;
 		((func_t)0x527FD0)(this, pl);
+	}
+
+	double getTime() const
+	{
+		return frameStart;
+	}
+
+	uint32_t getTimeMs() const
+	{
+		return (uint32_t)(getTime() * 1000);
 	}
 };
