1b2f9cc000844018f51eaf9df18cb83e7ac0359b
AltimorTASDK <altimormc@gmail.com>
2024-01-16 22:22:04 -0500

    call ensure_tty_input_thread regardless of AttachConsole/AllocConsole result

--- src/plugins/wineFix/wineFix.cpp
+++ src/plugins/wineFix/wineFix.cpp
@@ -4,12 +4,13 @@
 void WineFixPlugin::hook_SimWinConsolePlugin_consoleCallback_allocConsole(CpuState &cs)
 {
 	// Try using the parent console instead of spawning a window.
-	if (AttachConsole(ATTACH_PARENT_PROCESS) || AllocConsole()) {
-		// This calls ensure_tty_input_thread on Wine, which doesn't happen
-		// automatically after AttachConsole/AllocConsole. Normally reading
-		// from stdin would call it, but because T1 checks for unread input
-		// with GetNumberOfConsoleInputEvents before reading stdin, that
-		// never happens.
-		ReadFile(GetStdHandle(STD_INPUT_HANDLE), nullptr, 0, nullptr, nullptr);
-	}
+	if (!AttachConsole(ATTACH_PARENT_PROCESS))
+		AllocConsole();
+
+	// This calls ensure_tty_input_thread on Wine, which doesn't happen
+	// automatically after AttachConsole/AllocConsole. Normally reading
+	// from stdin would call it, but because T1 checks for unread input
+	// with GetNumberOfConsoleInputEvents before reading stdin, that
+	// never happens.
+	ReadFile(GetStdHandle(STD_INPUT_HANDLE), nullptr, 0, nullptr, nullptr);
 }
